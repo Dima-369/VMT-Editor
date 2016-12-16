823f73e621843e9e5518e132d31017db050f60fe
Dmytro Butemann <dmytro.butemann@gmail.com>
2016-12-16 23:56:15 +0100

    Undef Windows ERROR marco to silence warning

--- src/logging/logging.h
+++ src/logging/logging.h
@@ -1,12 +1,14 @@
-#ifndef LOGGING_H
-#define LOGGING_H
+#pragma once
 
 #include "ui_mainwindow.h"
 
 /*!
  * As long as the UI parameter is always called ui, we can safely use this
  * macro to save some keystrokes!
+ *
+ * Some Windows library also defines ERROR, but we want this.
  */
+#undef ERROR
 #define ERROR(s_) logging::error(s_, ui);
 
 namespace logging {
@@ -18,5 +20,3 @@ namespace logging {
 void error(const QString &s, Ui::MainWindow *ui);
 
 } // namespace logging
-
-#endif // LOGGING_H
