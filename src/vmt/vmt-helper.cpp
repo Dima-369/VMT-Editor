eb7b2135dc934e9fc80d9b2e71922a1a0f64892d
Jan Gortnar <gortnarj@gmail.com>
2016-12-17 05:23:37 +0100

    Fix typo

--- src/vmt/vmt-helper.cpp
+++ src/vmt/vmt-helper.cpp
@@ -217,7 +217,7 @@ void utils::parseTexture(const QString &parameter, const QString &value,
 	}
 
 	if (vmt.state.gameDirectory != 0 && !vmt.state.gameDirectory->exists(
-			"material/" + s + ".vtf")) {
+			"materials/" + s + ".vtf")) {
 
 		ERROR(parameter + " file: \"" + s + ".vtf\" does not exist!")
 	}
