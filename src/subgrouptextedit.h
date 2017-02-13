463a3de95bd7114faa453006602093c81020addd
Dmytro Butemann <dmytro.butemann@gmail.com>
2017-02-13 06:52:10 +0100

    Add basic line numbers to proxies

--- src/subgrouptextedit.h
+++ src/subgrouptextedit.h
@@ -1,35 +1,66 @@
 #pragma once
 
-#include <QTextEdit>
+#include <QPlainTextEdit>
+#include <QCompleter>
+#include <QKeyEvent>
+#include <QAbstractItemView>
+#include <QtDebug>
+#include <QApplication>
+#include <QModelIndex>
+#include <QAbstractItemModel>
+#include <QScrollBar>
+#include <QFile>
+#include <QStringListModel>
+#include <QMimeData>
+#include <QTextDocumentFragment>
+#include <QFileSystemModel>
+#include <QDirModel>
+#include <QTableView>
+#include <QHeaderView>
 
+#include "vmtparser.h"
 #include "utilities.h"
 #include "highlighter.h"
 
-class SubGroupTextEdit : public QTextEdit
+class LineNumberArea;
+
+class SubGroupTextEdit : public QPlainTextEdit
 {
 	Q_OBJECT
-
 public:
+	SubGroupTextEdit(QWidget* parent = NULL);
 
-	SubGroupTextEdit( QWidget* parent = NULL );
+	void lineNumberAreaPaintEvent(QPaintEvent* event);
+	int lineNumberAreaWidth();
 
 protected:
+	void keyPressEvent(QKeyEvent* event);
+	void focusInEvent(QFocusEvent* event);
+	void resizeEvent(QResizeEvent* event) override;
 
-	void keyPressEvent( QKeyEvent* event );
-
-	void focusInEvent( QFocusEvent*event );
-
-	void insertFromMimeData( const QMimeData* source );
+	void insertFromMimeData(const QMimeData* source);
 
 private slots:
-
-	void insertCompletion( const QString& completion );
+	void insertCompletion(const QString& completion);
+	void updateLineNumberAreaWidth();
+	void updateLineNumberArea(const QRect& rect, int dy);
 
 private:
+	QCompleter* mCompleter;
+	LineNumberArea* lineNumberArea;
 
 	QString textUnderCursor() const;
+};
+
+class LineNumberArea : public QWidget
+{
+public:
+	LineNumberArea(SubGroupTextEdit* editor);
+	QSize sizeHint() const override;
+
+protected:
+	void paintEvent(QPaintEvent *event) override;
 
 private:
-
-	QCompleter* mCompleter;
+	SubGroupTextEdit* editor;
 };
