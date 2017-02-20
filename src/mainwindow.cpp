8ca91e552ba6551607949eb66d3307df47ce9410
Jan Gortnar <gortnarj@gmail.com>
2017-02-20 02:51:27 +0100

    Change vmt preview font to consolas

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -372,13 +372,13 @@ MainWindow::MainWindow(QString fileToOpen, QWidget* parent) :
 	//----------------------------------------------------------------------------------------//
 
 	QFont font;
-		font.setFamily("Segoe UI");
+		font.setFamily("Consolas");
 		font.setStyleHint(QFont::Monospace);
 		font.setFixedPitch(true);
 		font.setPointSize(9);
 
 	QFontMetrics metrics(font);
-	ui->vmtPreviewTextEdit->setTabStopWidth(4 * metrics.width(' '));
+	ui->vmtPreviewTextEdit->setTabStopWidth(2 * metrics.width(' '));
 
 	//----------------------------------------------------------------------------------------//
 
